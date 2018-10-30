634f674aa980d7609a4346b82115fdfe9aec3b4d
Crist <pancrafterszerocrafters@gmail.com>
2018-10-31 07:53:54 +1100

    Update rubot.js

--- rubot.js
+++ rubot.js
@@ -26,7 +26,7 @@ bot.on("message", async message => {
   }
   if(cmd === `${prefix}help`){
     let embed = new discord.RichEmbed()
-    .addField(`Commands List (7):`, `>test`, `>help`, `>botinfo`, `>suggest`, `>play`, `leave`, `repo`);
+    .addField("Commands List (7):", "`>test`, `>help`, `>botinfo`, `>suggest`, `>play`, `leave`, `repo`");
     message.channel.send(embed);
   }
   if(cmd === `${prefix}botinfo`){
