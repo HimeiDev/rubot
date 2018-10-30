a6029c4f672836899c933d4484d797d1b9753054
Crist <pancrafterszerocrafters@gmail.com>
2018-10-31 07:25:54 +1100

    Update rubot.js

--- rubot.js
+++ rubot.js
@@ -50,11 +50,11 @@ bot.guilds.get("496990452369588224").channels.get("506885418512285699").send(sug
    if (!message.member.voiceChannel) return message.channel.send('You are not connected to a voice channel!');
     if (message.guild.me.voiceChannel) return message.channel.send('I am already connected to a voice channel!');
     if (!args[0]) return message.channel.send('You must give me a url to the song you would like to listen to!');
-    let valid = await ytdl.validateURL(args[0]);
-    if (!valid) return message.channel.send('This URL is invalid!');
+    let validate = await ytdl.validateURL(args[0]);
+    if (!validate) return message.channel.send('This URL is invalid!');
     let info = await ytdl.getInfo(args[0]);
     let connect = await message.member.voiceChannel.join();
-    let connection = await connection.playStream(ytdl(args[0], { filter: 'audioonly' }));
+    let dispatcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly' }));
     message.channel.send(`Now Playing: ${info.title}`);
  }
   if(cmd === `${prefix}leave`){
